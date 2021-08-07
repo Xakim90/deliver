f4efa73bbfdcca4a26ed2c9dd506b8e849f76cd5
xakim90 <xakim90@gmail.com>
2021-08-07 12:24:38 +0500

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return Book::all();
+    }
+    public function show(Book $product)
+	{
+	    return $product;
+	}
+
+	public function store(Request $request)
+	{
+	   $this->validate($request, [
+        'author' => 'required|string',
+        'title' => 'required|unique:products|max:255',
+        'file' => 'binary',
+        'published' => 'integer',
+		'category_id' => 'integer',
+        'description' => 'string',
+        'price' => 'integer',
+        'availability' => 'boolean',
+		'photo' => 'binary',
+		'selected' => 'boolean',
+        'status' => 'string',
+        'count' => 'integer'
+    ]);
+	    $book = Book::create($request->all());
+
+	    return response()->json($book, 201);
+	}
+
+	public function update(Request $request, Book $book)
+	{
+
+	    $book->update($request->all());
+
+	    return response()->json($book, 200);
+	}
+
+	public function delete(Book $book)
+	{
+	    $book->delete();
+
+	    return response()->json(null, 204);
+	}
+}
---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportAuthController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('users/profile',[UserProfileController::class, 'show'])->name('profile');
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+Route::post('me', [PassportAuthController::class, 'me']);
+
+Route::get('books', 'BookController@index');
+Route::get('books/{id}', 'BookController@show');
+Route::post('books','BookController@store');
+Route::put('books/{id}','BookController@update');
+Route::delete('books/{id}', 'BookController@delete');
+// Route::get('/users/{id}', function ($id) {
+//     return 'User '.$id;
+// });
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return "Post ".$postId." ".$commentId;
+// });
+// Route::get('/users/{name?}', function ($name = null) {
+//     return $name;
+// });
+//Route::redirect('/here', '/there', 301);
+// Route::get('/user/{id}', function ($id) {
+//     return 'User '.$id;
+// })->where('id', '[0-9]+');
