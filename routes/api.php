2651397f0e31ccc6884e43c2847e7d1ae5cac046
xakim90 <xakim90@gmail.com>
2021-11-13 20:03:35 +0500

    fixed

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassportAuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\FileUpload;
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,11 @@ Route::get('books/{id}', [BookController::class, 'show']);
 Route::post('books',[BookController::class, 'store']);
 Route::put('books/{id}',[BookController::class, 'update']);
 Route::delete('books/{id}', [BookController::class, 'delete']);
+Route::get('product', [ProductController::class, 'index']);
+Route::get('product/{id}', [ProductController::class, 'show']);
+Route::post('product',[ProductController::class, 'store']);
+Route::put('product/{id}',[ProductController::class, 'update']);
+Route::delete('product/{id}', [ProductController::class, 'delete']);
 Route::get('/upload-file', [FileUpload::class, 'createForm']);
 
 Route::post('/upload-file', [FileUpload::class, 'fileUpload'])->name('fileUpload');
