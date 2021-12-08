d782a98db5722a7094bd975af9f154053e08e0d8
Markiloff <zuxriddinovsirojidin@gmail.com>
2021-12-08 15:29:57 +0500

    gg

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import '../style.css'
+import background from '../images/NewYork.jpg'
 
 const Login = () => {
-    const {register, handleSubmit} = useForm()
+    const { register, handleSubmit } = useForm()
     const [result, setResult] = useState("")
     console.log(result)
 
@@ -11,21 +13,31 @@ const Login = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-                className="p-2 px-16 border rounded outline-none"
-                {...register("email")}
-                placeholder="Email"
-            />
-            <br />
-            <input
-                className="p-2 px-16 border rounded outline-none"
-                {...register("password")}
-                placeholder="Password"
-            />
-            <br />
-            <input type="submit" />
-        </form>
+        <div style={{ backgroundImage: `url(${background})` }}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid grid-cols-12 h-screen">
+                    <div className="col-span-4"></div>
+                    <div className="border mt-28 rounded shadow-2xl w-96 h-96 text-center">
+                        <br />
+                        <br />
+                        <div className="text-2xl h1">Login Page</div>
+                        <br />
+                        <br />
+                        <input
+                            className="border rounded border-red-300 outline-none h-10 px-7"
+                            {...register("email")}
+                            placeholder="Email"
+                        />
+                        <input
+                            className="my-5 border rounded border-red-300 outline-none h-10 px-7"
+                            {...register("password")}
+                            placeholder="Password"
+                        />
+                        <input className="bg-red-500 rounded text-white h-10 px-20 cursor-pointer" type="submit" />
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
