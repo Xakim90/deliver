13694f8831d2bcad33fa08420e8978342e9644f9
Sardor1498 <tsardor1498@gmail.com>
2021-12-09 23:33:24 +0500

    post request

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,23 +16,23 @@ const Login = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="grid grid-cols-12 h-screen">
                     <div className="col-span-4"></div>
-                    <div className="border mt-28 rounded shadow-2xl w-96 h-72 text-center">
+                    <div className="col-span-4 border mt-28 rounded shadow-2xl h-72 text-center">
                         <br />
                         <div className="text-2xl">Login Page</div>
                         <br />
                         <input
-                            className="rounded border border-red-300 outline-none h-10 w-72 px-4"
+                            className="rounded border border-red-300 outline-none h-10 w-80 p-5"
                             {...register("email")}
                             type="text"
                             placeholder="Email"
                         />
                         <input
-                            className="my-5 border rounded border-red-300 outline-none h-10 w-72 px-4"
+                            className="my-5 border rounded border-red-300 outline-none h-10 w-80 p-5"
                             {...register("password")}
                             type="password"
                             placeholder="Password"
                         />
-                        <input className="bg-black rounded text-white h-10 px-20 cursor-pointer" type="submit" />
+                        <input className="bg-black hover:bg-blue-600 rounded text-white h-10 px-20 cursor-pointer" type="submit" />
                     </div>
                     <div className="col-span-4"></div>
                 </div>
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { useForm } from 'react-hook-form'
 import background from '../images/s.jpg'
 
+
+
+let url = 'https://deliver-project.herokuapp.com/api/register'
+
 const Register = () => {
 
-    const { register, handleSubmit } = useForm()
-    const [result, setResult] = useState("")
-    console.log(result)
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const [loading, setLoading] = useState(false)
+
 
     const onSubmit = (data) => {
-        setResult(JSON.stringify(data))
+        loading === false ? setLoading(true) : setLoading(false)
+        axios.post(url, data)
+            .then(res => {
+                console.log(res.data)
+            }).catch(error => {
+                console.log(error.data)
+            })
     }
 
     return (
@@ -18,27 +29,39 @@ const Register = () => {
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="grid grid-cols-12 h-screen">
                         <br />
-                        <div className=" col-span-5 border border-blue-500 mt-28 rounded h-96 text-center">
-                            <br />
+                        <div className="col-span-5 border border-blue-500 mt-28 rounded h-96 text-center">
                             <br />
                             <div className="text-2xl text-white">Register</div>
                             <br />
                             <input
-                                className="border rounded bg-black outline-none h-10 w-96 p-5"
-                                {...register("username")}
-                                placeholder="UserName"
+                                className="border rounded bg-white outline-none h-10 w-96 p-5"
+                                {...register("name")}
+                                placeholder="Name"
+                                required
                             />
                             <input
-                                className="my-5 border rounded bg-black outline-none h-10 w-96 p-5"
+                                className="my-5 border rounded bg-white outline-none h-10 w-96 p-5"
                                 {...register("email")}
                                 placeholder="Email"
+                                required
                             />
                             <input
-                                className=" border rounded bg-black outline-none h-10 w-96 p-5"
+                                className="border rounded bg-white outline-none h-10 w-96 p-5"
                                 {...register("password")}
                                 placeholder="Password"
+                                required
                             />
-                            <input className="my-5 bg-blue-500 rounded text-white h-10 px-20 cursor-pointer" type="submit" />
+                            {
+                                loading ? (
+                                    <div
+                                        class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700"
+                                    >
+                                        <p>Loading...</p>
+                                    </div>
+                                ) : (
+                                    <input className="my-5 bg-blue-500 rounded text-white h-10 px-20 cursor-pointer" type="submit" />
+                                )
+                            }
                         </div>
                     </div>
                 </form>
